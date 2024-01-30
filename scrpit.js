6258b2ee589477580a34d267ebf65b3dac455e8a
José William <114762784+williamC137@users.noreply.github.com>
2024-01-30 20:30:01 -0300

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -2,6 +2,6 @@ document.getElementById("name").value = "José William"
 document.getElementById("surname").value = "Almeida da Silva"
 document.getElementById("study_area").value = "Ciência da Computação"
 document.getElementById("instution").value = "Estácio FIC - Parangaba"
-document.getElementById("field_experience").value = "Possuo experiência na área de suporte técnico ao usuário, obtida por meio do estágio em Help Desk na empresa ISGH(Instituto de Gestão e Saúde e Gestão Hospitalar). Atualmente, no início de 2024 estou na mesma empresa que iniciei. Porém, no cargo de estagiário de Banco de Dados, onde auxilio na criação e manipulação de tabelas utilizando a plataforma gratuita DBeaver. Além disso, possuo conhecimento em desenvolovimento web e desejo aprimorar meus conhecimentos em programação para alcançar meu primeiro emprego na área."
+document.getElementById("field_experience").value = "Possuo experiência na área de suporte técnico ao usuário, obtida por meio do estágio em Help Desk na empresa ISGH(Instituto de Gestão e Saúde e Gestão Hospitalar). Atualmente, no início de 2024 estou na mesma empresa que iniciei. Porém, no cargo de estagiário de Banco de Dados, onde auxilio na criação e manipulação de tabelas utilizando a plataforma gratuita DBeaver. Além disso, possuo conhecimento em desenvolvimento web e desejo aprimorar meus conhecimentos em programação para alcançar meu primeiro emprego na área."
 
 
